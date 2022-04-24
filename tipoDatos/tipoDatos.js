3a8453795560ee9e285e2d829c1f5e2472ee58fc
Jose David <jose7312000@gmail.com>
2022-04-23 21:30:43 -0500

    Tipo de datos

---
+++ tipoDatos/tipoDatos.js
@@ -0,0 +1,23 @@
+//Valores primitivos
+
+//Entero
+20
+
+//String o cadena de texto
+"Hola"
+
+//Booleanos
+true
+false
+
+//Valores vacios
+null
+undefined
+
+//Valores de objeto:
+
+//String
+[1, 2, 3]
+
+//Objeto
+{nombre: "Jose"}
