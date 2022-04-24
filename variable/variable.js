6da060d571c25fe27c55d4177de389783eea1d04
Jose David <jose7312000@gmail.com>
2022-04-23 21:41:06 -0500

    Variable con sus respectivos tipos de datos

---
+++ variable/variable.js
@@ -0,0 +1,16 @@
+//Las variables se pueden conciderar como un espacio que se reserva en memoria que se espera utilizar en el futuro
+
+//String
+var nombre = "Jose";
+
+//Entero
+var num = 2;
+
+//Array
+var elementos = ["Pc", "Carro"];
+
+//Objeto
+var personas ={
+    nombre: "jose ",
+    edad: 20
+}
