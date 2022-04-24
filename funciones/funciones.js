35eafbe23c017718cc0cc6f109a537ce19311ec4
Jose David <jose7312000@gmail.com>
2022-04-23 21:47:41 -0500

    Funciones

---
+++ funciones/funciones.js
@@ -0,0 +1,12 @@
+//La funciones son un bloque de codigo reutilizable existen 2 tipos de funcion en js
+
+//Declaratativas
+function Test(){
+    return "Hola";
+}
+
+//Expresivas o anonimas
+var miFuncion = function(a, b){
+    return a +b;
+}
+
