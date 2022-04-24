d43a46c78b3f86c19934a2f77145c6d95789492b
Jose David <jose7312000@gmail.com>
2022-04-23 22:48:19 -0500

    V2 operadores

--- operadores/operadores.js
+++ operadores/operadores.js
@@ -1,31 +1,55 @@
-//Operadores binarios:
-3 + 2 //Suma
-50 - 10 // Resta
-10 * 20 //Multiplicación
-20 / 2 //División
+// OPERADORES
 
-"Diego " + "De Granda" //concatenación de strings
+// ARITMETICOS
 
-//Operadores unitarios:
-!false //negación de la negación = true
+// Suma a + b,  afirmación unitaria +a : Positivo, concatenación "a" + "la" : "ala"
+//Resta a - b, negación unitaria -a : Negativo
+// Producto a * b
+// Divisor a / b
+// Residuo a % b
+// Potencia a ** b, a elevado a b
+// Incremento (suma uno) a++ : a = a + 1
+// Decremento (resta uno) a-- : a = a - 1
 
-//Operadores de asignación:
-var a = 1; //Asignamos un valor a la variable
+// ASIGNACIÓN
 
-//Operadores para comparar:
-3 == "3"; //Compara los valores y devuelve "true" en este caso
+// Asignación a = b
+// Asignación de adición  a += b : a = a + b
+// Asignación de sustracción  a -= b : a = a - b
+// Asignación de multiplicación  a *= b : a = a * b
+// Asignación de división  a /= b : a = a / b
+// Asignación de residuo  a %= b : a = a % b
+// Asignación de potencia  a **= b : a = a ** b
+// Asignación de desplazamiento a la izquierda  a <<= b : a = a << b
+// Asignación de desplazamiento a la derecha  a >>= b : a = a >> b
+// Asignación sin signo de desplazamiento a la derecha  a >>>= b : a = a >>> b
+// Asignación AND  a &= b : a = a & b
+// Asignación XOR  a ^= b : a = a ^ b
+// Asignación OR  a |= b : a = a | b
 
-3 === "3"; //Compara y valida los tipos y valores. Devuelve "falso" en este caso
+// COMPARACIÓN
 
-5 < 3 //Compara y valida si el 5 es menor a 3
-5 > 3 //Compara y valida si el 5 es mayor a 3
-5 <= 6 //Compara y valida si el 5 es menor o igual al 6
-5 >= 6 //Compara y valida si el 5 es mayor o igual al 6
+// Igualdad a == b, a tiene el mismo valor que b
+// Distinto a != b, a tiene un valor diferente a b
+// Identidad a === b igual valor igual tipo de dato
+// Sin Identidad a !== b igual valor o igual tipo de dato
+// Mayor que a > b
+// Mayor o igual que a >= b
+// Menor que a < b
+// Menor o igual que a <= b
 
-a && b //Valida si ambas variables son verdaderas para que se cumpla la condición
-a || b //Aquí se cumple la condición si alguna de las dos variables es verdadera
+// LOGICOS
 
-var edad = 40
-edad++ //Incrementa el valor en 1
+// AND, Y, a && b : a y b
+// OR, O, a || b : a o b
+// NOT, Negación, a = true : !a = false
 
-edad += 2 //Incrementa el valor por 2
+// LOGICOS A NIVEL DE BITS
+
+// Desplazamiento a la izquierda  a << b
+// Desplazamiento a la derecha  a >> b
+// Desplazamiento a la derecha sin signo  a >>> b
+// AND  a & b
+// XOR  a ^ b
+// OR  a | b
+// NOT  a | b```
