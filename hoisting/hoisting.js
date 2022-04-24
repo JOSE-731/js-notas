3a868c64f7f2f47536c5a70412a1a8660815dd9e
Jose David <jose7312000@gmail.com>
2022-04-23 22:21:47 -0500

    Hoisting

---
+++ hoisting/hoisting.js
@@ -0,0 +1,7 @@
+//Hoisting es cuando las variables y funciones se procesan antes de ejecutar cualquier tipo de codigo
+//en otras palabras seria cuando llamamos una funcion o variable antes de declararla
+
+//Ejemplo
+console.log(data);
+
+var data = "Hola"
