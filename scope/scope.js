9dc92aff7f817f06578e55f2e1e8df22a2bc0977
Jose David <jose7312000@gmail.com>
2022-04-23 22:09:34 -0500

    Scope

---
+++ scope/scope.js
@@ -0,0 +1,16 @@
+//Alcance que tiene una variable y que si tenemos acceso a ella o no existen dos scope:
+
+//Scope global
+
+//Todas las funciones, variables que hemos definido ejemplo:
+
+//Se puede acceder desde cualquier parte del documento incluso dentro de una funcion
+var data= 20;
+
+//Scope local
+function Test(){
+
+    //Scope local, solo se puede acceder dentro de la funcion
+    var name = 21;
+
+}
