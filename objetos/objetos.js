598905b4b6181af2b9a5a765cb81b7291e56713c
Jose David <jose7312000@gmail.com>
2022-04-28 22:02:45 -0500

    Objeto con contructor

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -15,5 +15,22 @@ var miAuto = {
 }
 
 //Para acceder a un objeto utilizamos
-
 miAuto.año
+
+//Objeto con contructor:
+function auto (MARCA, MODELO, ANNIO){
+    this.marca = MARCA;
+    this.modelo = MODELO;
+    this.annio = ANNIO;
+  }
+  var autos = [];
+  for(let i = 0 ; i < 30 ; i++){
+    var marca = prompt("Ingresa la marca del auto");
+    var modelo = prompt("Ingresa el modelo del auto");
+    var annio = prompt("Ingresa el año del auto");
+    autos.push(new auto (marca, modelo, annio));
+  }
+  
+  for(let i = 0 ; i < autos.length ; i++){
+    console.log(autos[i]);
+  }
